46b1cb0867c2b69568d1a12a733b2065e690c970
James Sherry <james.m.sherry@googlemail.com>
2022-07-06 21:26:34 +0100

    last code

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const app = express();
 
-const {
-  PORT = 3333,
-  MONGODB_URI='mongodb://localhost:27017/cars5'
-} = process.env;
+const { PORT = 3333, MONGODB_URI = "mongodb://localhost:27017/cars5" } =
+  process.env;
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -17,11 +14,11 @@ console.log("hello world");
 
 (async () => {
   try {
-    const conn = await mongoose.connect(MONGODB_URI)
+    const conn = await mongoose.connect(MONGODB_URI);
     // console.log("🚀 ~ file: index.js ~ line 21 ~ conn", conn);
-    mongoose.connection.on('error', (err) => {
+    mongoose.connection.on("error", (err) => {
       console.log(err);
-    })
+    });
   } catch (err) {
     console.log(`Connection error`, err);
   }
@@ -48,38 +45,45 @@ const carSchema = new Schema({
   },
 });
 
-const Car = mongoose.model('Car', carSchema);
-
+const Car = mongoose.model("Car", carSchema);
 
 // const cars = [];
 
 app.get("/api/v1/cars", (req, res, next) => {
   Car.find({}).exec((err, cars) => {
-    res.json(cars);
-  })
+    if (err) return res.status(500).send(err);
+
+    res.status(200).json(cars);
+  });
 });
 
 app.post("/api/v1/cars", (req, res, next) => {
   console.log(req.body);
-  const newCar = { _id: cars.length, ...req.body };
-  cars.push(newCar);
-  res.sendStatus(201);
-  res.status(201).json(newCar);
+  const newCar = new Car(req.body);
+
+  newCar.save((err, car) => {
+    if (err) return res.status(500).send(err);
+    res.status(201).json(car);
+  });
+});
+
+app.put("/api/v1/cars/:id", (req, res, next) => {
+  const carId = req.params.id;
+  const updates = req.body;
+
+  Car.updateOne({ _id: carId }, updates, (err) => {
+    if (err) return res.status(500).send(err);
+    res.sendStatus(200);
+  });
 });
 
 app.delete("/api/v1/cars/:id?", (req, res, next) => {
   console.log(req.params.id);
-  console.log(req.query)
   const carId = req.params.id;
-  const index = cars.findIndex((car) => {
-    return car._id === Number(carId);
-  });
-  console.log("index", index);
-  if (index === -1) {
-    return res.sendStatus(404);
-  }
-  cars.splice(index, 1);
-  res.sendStatus(204);
+  Car.remove({ _id: carId}, (err) => {
+    if (err) return res.status(500).send(err);
+    res.sendStatus(204);
+  })
 });
 
 app.get("/redirect", (req, res) => {
